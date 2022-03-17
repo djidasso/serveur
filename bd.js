ba153ccd8dc04921bbd409ff8d0fdebf4ac44564
diangana <39535013+diangana@users.noreply.github.com>
2022-03-17 16:27:55 +0100

    f

--- bd.js
+++ bd.js
@@ -1,8 +1,8 @@
 var mysql   = require('mysql');
 var connection = mysql.createPool({
-    host     : 'localhost',
-    user     : 'root',
-    password : '',
+    host     : 'eu-cdbr-west-02.cleardb.net',
+    user     : 'b0a78a0f337a6d',
+    password : 'c258d06e',
     database : 'djida_bd'
 });
 module.exports=connection;
